f63c4dced8c4e7053687e6974cd334cafb296997
aemmanuella <anthonyemmanuella3@gmail.com>
2026-05-01 13:24:27 +0100

    feat: implement login API

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,58 @@
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request)
+{
+    // all logic here
+    const body = await req.json();
+    const {email, password} = body;
+
+    if (!email || !password)
+    {
+        return Response.json(
+            { error: "Email and password are required" },
+            { status: 400 }
+        );
+    }
+
+    const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+    if(user.length === 0) {
+        return Response.json(
+            {error: "Invalid credentials"},
+            {status: 401}
+        );
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+        password,
+        user[0].passwordHash
+    );
+
+    if(!isPasswordValid) 
+    {
+        return Response.json(
+            {error: "Invalid credentials"},
+            {status: 401}
+        );
+    }
+
+    return Response.json(
+        {
+            message: "Login successful",
+            user: {
+                id: user[0].id,
+                name: user[0].name,
+                email: user[0].email
+            }
+        },
+        { status: 200 }
+    );
+}
+
