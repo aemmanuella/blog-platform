a211387adc5016369f4a0be2af862e5c520861d2
aemmanuella <anthonyemmanuella3@gmail.com>
2026-05-03 12:39:25 +0100

    refactor: add Zod validation to edit profile

--- src/app/api/user/edit/route.ts
+++ src/app/api/user/edit/route.ts
@@ -1,19 +1,32 @@
+import { z } from "zod";
 import { db } from "@/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
+const editProfileSchema = z.object({
+    userId: z.number(),
+    name: z.string().min(1, {message: "name cannot be less than 1 character"}),
+    email: z.string().email({message: "Invalid email format"}),
+});
+
 export async function POST(req: Request) 
 {
     const body = await req.json();
-    const { userId, name, email } = body;
+   
+    const result = editProfileSchema.safeParse(body);
 
-    if (!userId || !name || !email)
+    if(!result.success)
     {
         return Response.json(
-            {error: "Missing fields"},
-            {status: 400}
+            {
+                error: "Invalid input",
+                details: result.error.flatten().fieldErrors,
+            },
+            { status: 400 }
         );
     }
+
+    const { userId, name, email } = result.data;
 
     await db
     .update(users)
